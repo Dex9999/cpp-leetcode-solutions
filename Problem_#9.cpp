b968d4827f049c3dc74232e4079fbdec2d29a750
Dex9999 <95934184+Dex9999@users.noreply.github.com>
2023-11-26 18:35:19 -0500

    Create Problem_#9.cpp

---
+++ Problem_#9.cpp
@@ -0,0 +1,30 @@
+//9. Palindrome Number
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        std::vector<char> charArray;
+        std::string string = std::to_string(x);
+        
+        for(int i = 0; i < string.length(); ++i){
+            charArray.insert(charArray.begin(), string[i]);
+        }
+        std::string str(charArray.begin(), charArray.end());
+
+        
+        if(str == string){
+            return true;
+        } else {
+            return false;
+        }
+    }
+};
+
+/*
+Stats
+
+Runtime: 23ms
+Beats 5.06% of users with C++
+Memory: 8.95 MB
+Beats 5.46% of users with C++
+*/
